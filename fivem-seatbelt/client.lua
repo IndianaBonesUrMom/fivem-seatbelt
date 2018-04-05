39f9c714bc48db5a451db29c4728e26dbe1556ef
Arkansas Tom Jones <atte.kuosmanen@edu.savonia.fi>
2018-04-05 13:38:40 +0300

    Update client.lua

--- fivem-seatbelt/client.lua
+++ fivem-seatbelt/client.lua
@@ -17,7 +17,9 @@ Citizen.CreateThread(function()
 		local car = GetVehiclePedIsIn(ped)
 		
 		if car ~= 0 and IsCar(car) then --jos pelaaja on autossa
+			
 			if beltOn then DisableControlAction(0, 75) end
+				
 			wasInCar = true 
 			speedBuffer[2] = speedBuffer[1]
 			speedBuffer[1] = GetEntitySpeed(car)
