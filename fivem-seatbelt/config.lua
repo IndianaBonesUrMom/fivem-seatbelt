d930a63c6dc3892708382267621a756b4dbad289
Arkansas Tom Jones <atte.kuosmanen@edu.savonia.fi>
2018-04-29 15:15:57 +0300

    Update config.lua

--- fivem-seatbelt/config.lua
+++ fivem-seatbelt/config.lua
@@ -1,4 +1,4 @@
 Cfg             = {}
-Cfg.DiffTrigger = 0.255
+Cfg.DiffTrigger = 0.255 
 Cfg.MinSpeed    = 19.25 --THIS IS IN m/s
 Cfg.Strings     = { belt_on = 'Turvavyö ^5 kiinni^0.', belt_off = 'Turvavyö ^1 auki^0.' }
